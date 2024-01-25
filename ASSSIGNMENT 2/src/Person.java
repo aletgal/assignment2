c4e161ed3e465493af80a5e4491eabf1bac953b5
Adlet Baigaliev <153578967+aletgal@users.noreply.github.com>
2024-01-25 23:09:02 +0600

    Add files via upload

--- ASSSIGNMENT 2/src/Person.java
+++ ASSSIGNMENT 2/src/Person.java
@@ -1,9 +1,8 @@
-public abstract class Person {
+public class Person implements Payable, Comparable<Person> {
     private static int nextId = 1;
     private String name;
     private String surname;
     private int id;
-
 
     public Person(){
         this.id = nextId++;
@@ -35,4 +34,22 @@ public abstract class Person {
         return surname;
     }
 
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + ": " + id + ". " + name + " " + surname;
+    }
+
+    @Override
+    public double getPaymentAmount() {
+        return 0.00;
+    }
+
+    @Override
+    public int compareTo(Person pers) {
+        return Double.compare(this.getPaymentAmount(), pers.getPaymentAmount());
+    }
+
+    public String getPosition() {
+        return "Student";
+    }
 }
